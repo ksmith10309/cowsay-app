82a5522d45ce85aff83dc1406507d6116a033a8d
Katherine Smith <katherine.lo.smith@gmail.com>
2019-03-04 14:57:33 -0800

    Implement cowsay with next.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const next = require('next')
+const cowsay = require('cowsay')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+.then(() => {
+  const server = express()
+
+  server.get('/', (req, res) => {
+    if (!req.query.text) req.query.text = 'Say what?'
+    app.render(req, res, '/', {cowsay: `${cowsay.say({f: req.query.f, text: req.query.text}) }`})
+  })
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(3000, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:3000')
+  })
+})
+.catch((ex) => {
+  console.error(ex.stack)
+  process.exit(1)
+})
